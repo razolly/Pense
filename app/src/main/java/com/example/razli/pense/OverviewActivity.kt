e06a1c10c1448db5a27c55517f6e0b73598be09f
Raz <razli1993@hotmail.com>
2018-09-11 20:43:27 +0800

    First commit

---
+++ app/src/main/java/com/example/razli/pense/OverviewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.razli.pense
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OverviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_overview)
+    }
+}
